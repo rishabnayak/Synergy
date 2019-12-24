c920c0eb4a74ce8772f2acfb2aa43e6b7cb6d694
rishab2113 <rishab@bu.edu>
2019-12-24 11:03:34 -0500

    firebase init for functions, hosting and rules

---
+++ functions/index.js
@@ -0,0 +1,23 @@
+const admin = require("firebase-admin");
+const glob = require("glob");
+const files = glob.sync("./**/*.function.js", {
+  cwd: __dirname,
+  ignore: "./node_modules/**"
+});
+
+admin.initializeApp();
+
+//Deploys all the functions in the format separate_dir/thisfunc.function.js to firebase
+files.forEach(file => {
+  const functionModule = require(file);
+  const functionNames = Object.keys(functionModule);
+
+  functionNames.forEach(functionName => {
+    if (
+      !process.env.FUNCTION_NAME ||
+      process.env.FUNCTION_NAME === functionName
+    ) {
+      exports[functionName] = functionModule[functionName];
+    }
+  });
+});
